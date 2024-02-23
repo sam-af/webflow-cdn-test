7d9694d85b3ca43bc67ef2ff7ebd9a2889d05e47
sam <sam-af@afundi.iim>
2024-02-23 14:38:40 +0000

    test to see if chnages are instant

--- javascript/test.js
+++ javascript/test.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    alert('This is still from GitHub! But it has changed! One more Time!!!')
+    alert('This is still from GitHub! But it has changed! What the Heck!')
 });
